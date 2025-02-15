d9354239ae2a42d38e917e6c0db79541d6c9af49
OstrichWhite <rahimcsestudies@gmail.com>
2025-02-15 20:27:04 +0530

    🎉 init(configure): redux toolkit

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,11 @@
+import { Provider } from "react-redux"
+import { store } from "./redux/store"
+
 const App = () => {
   return (
-    <div className='text-3xl'>App</div>
+    <Provider store={store}>
+      <div className='text-3xl'>App</div>
+    </Provider>
   )
 }
 
