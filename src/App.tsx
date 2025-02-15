d5e62e7599061c82df2611b5a9cd84b57379aa79
OstrichWhite <rahimcsestudies@gmail.com>
2025-02-15 20:51:10 +0530

    🌈 style(pretty): rewrite

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,16 @@
-import { Provider } from "react-redux"
-import { store } from "./redux/store"
-import { ApolloProvider } from "@apollo/client"
-import client from "./graphql/apolloClient"
+import { Provider } from 'react-redux';
+import { store } from './redux/store';
+import { ApolloProvider } from '@apollo/client';
+import client from './graphql/apolloClient';
 
 const App = () => {
   return (
     <ApolloProvider client={client}>
       <Provider store={store}>
-        <div className='text-3xl'>App</div>
+        <div className="text-3xl">App</div>
       </Provider>
     </ApolloProvider>
-  )
-}
+  );
+};
 
-export default App
+export default App;
