b2040c76e5063cb5f8e391c5cf3a7489e48721ab
OstrichWhite <rahimcsestudies@gmail.com>
2025-02-15 20:43:03 +0530

    🎉 init(configure): graphql
    apollo graphql client

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import { Provider } from "react-redux"
 import { store } from "./redux/store"
+import { ApolloProvider } from "@apollo/client"
+import client from "./graphql/apolloClient"
 
 const App = () => {
   return (
-    <Provider store={store}>
-      <div className='text-3xl'>App</div>
-    </Provider>
+    <ApolloProvider client={client}>
+      <Provider store={store}>
+        <div className='text-3xl'>App</div>
+      </Provider>
+    </ApolloProvider>
   )
 }
 
