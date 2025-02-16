90a226026c54c2bfc51b53ae0146fe5ba0b853b7
OstrichWhite <rahimcsestudies@gmail.com>
2025-02-16 15:56:44 +0530

    ✨ feat(layout): Navbar item
    Added basic navbar items

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -1,15 +1,16 @@
 import { LuBell, LuSearch, LuSettings, LuUserRound } from 'react-icons/lu';
+const rounded = 'p-1 rounded-full h-6 w-6 hover:bg-gray-300';
 
 const Navbar = () => (
-  <nav className="bg-gray-500 flex justify-between items-center">
-    <div>
-      <LuSearch />
-      <input type="text" />
+  <nav className="flex justify-between items-center px-6 py-3 bg-white shadow-md">
+    <div className="flex items-center space-x-3">
+      <LuSearch className='text-gray-500'/>
+      <input type="text" placeholder='Search' className='border-none outline-none bg-transparent'/>
     </div>
-    <div>
-      <LuBell />
-      <LuUserRound />
-      <LuSettings />
+    <div className="flex items-center space-x-4">
+      <LuBell className={`${rounded} text-gray-500`} />
+      <LuSettings className={`${rounded} text-gray-500`} />
+      <LuUserRound className={`${rounded} text-gray-700`} />
     </div>
   </nav>
 );
