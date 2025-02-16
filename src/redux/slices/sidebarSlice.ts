8c11f6cdc03f15af2eecc0eb44e2ef2d11fe2c15
OstrichWhite <rahimcsestudies@gmail.com>
2025-02-16 18:14:11 +0530

    🌈 style(pretty): rewrite

--- src/redux/slices/sidebarSlice.ts
+++ src/redux/slices/sidebarSlice.ts
@@ -1,25 +1,25 @@
-import { createSlice } from "@reduxjs/toolkit"
+import { createSlice } from '@reduxjs/toolkit';
 
 interface SidebarState {
-    expanded: boolean
+  expanded: boolean;
 }
 
 const initialState: SidebarState = {
-    expanded: false,
-}
+  expanded: false,
+};
 
 const sidebarSlice = createSlice({
-    name: 'sidebar',
-    initialState,
-    reducers: {
-        toggleSidebar: (state) => {
-            state.expanded = !state.expanded
-        },
-        setSidebarState: (state, action) => {
-            state.expanded = action.payload
-        }
-    }
-})
+  name: 'sidebar',
+  initialState,
+  reducers: {
+    toggleSidebar: (state) => {
+      state.expanded = !state.expanded;
+    },
+    setSidebarState: (state, action) => {
+      state.expanded = action.payload;
+    },
+  },
+});
 
-export const { toggleSidebar, setSidebarState } = sidebarSlice.actions
-export default sidebarSlice.reducer
+export const { toggleSidebar, setSidebarState } = sidebarSlice.actions;
+export default sidebarSlice.reducer;
