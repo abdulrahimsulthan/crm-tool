de8633cc74834a08fbdf0e18927a3f4786516b9b
OstrichWhite <rahimcsestudies@gmail.com>
2025-02-16 18:11:18 +0530

    ✨ feat(layout): layout items
    Basic style for the layout done for all pages

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -10,13 +10,13 @@ const Layout = () => {
   );
 
   return (
-    <div className="flex h-screen bg-amber-300">
+    <div className="flex h-screen bg-gray-900">
       <Sidebar />
       <div
-        className={`flex1 flex flex-col bg-amber-600 transition-all duration-300 ${sidebarExpanded ? 'w-[90%]' : 'w-[calc(100%-60px)]'}`}
+        className={`flex1 flex flex-col transition-all duration-300 ${sidebarExpanded ? 'w-[90%]' : 'w-[calc(100%-60px)]'}`}
       >
         <Navbar />
-        <main className="p-6 bg-gray-100 ">
+        <main className="p-6 bg-gray-100 h-[calc(100vh-60px)] rounded-tl-2xl">
           <Outlet />
         </main>
       </div>
--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -1,16 +1,20 @@
 import { LuBell, LuSearch, LuSettings, LuUserRound } from 'react-icons/lu';
-const rounded = 'p-1 rounded-full h-6 w-6 hover:bg-gray-300';
+const rounded = 'p-1 rounded-full h-6 w-6 hover:bg-gray-300 text-white';
 
 const Navbar = () => (
-  <nav className="flex justify-between items-center px-6 py-3 bg-white shadow-md">
-    <div className="flex items-center space-x-3">
-      <LuSearch className='text-gray-500'/>
-      <input type="text" placeholder='Search' className='border-none outline-none bg-transparent'/>
+  <nav className="flex justify-between items-center px-6 py-3 bg-gray-900 shadow-md h-[60px]">
+    <div className="w-[30%] flex items-center space-x-3 border-1 border-gray-500 rounded-full px-4 py-2 text-white">
+      <LuSearch />
+      <input
+        type="text"
+        placeholder="Search"
+        className="border-none outline-none bg-transparent"
+      />
     </div>
     <div className="flex items-center space-x-4">
-      <LuBell className={`${rounded} text-gray-500`} />
-      <LuSettings className={`${rounded} text-gray-500`} />
-      <LuUserRound className={`${rounded} text-gray-700`} />
+      <LuBell className={`${rounded} `} />
+      <LuSettings className={`${rounded} `} />
+      <LuUserRound className={`${rounded}`} />
     </div>
   </nav>
 );
